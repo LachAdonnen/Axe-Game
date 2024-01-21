38e8e3a8d991ddd45dc82d5f5d96c7107acb7ea8
Alex McClain <alex.mcclain423@gmail.com>
2024-01-21 07:57:23 -0600

    Create the primary file for AxeGame with main

---
+++ AxeGame.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file AxeGame.cpp
+ * @brief This file contains the main entry point for the Axe Game.
+ * 
+ * This game is a simple introductory project for game development.
+ * It uses the raylib library for graphics and input handling.
+ * 
+ * @author Alex McClain
+ */
+#include "raylib.h"
+
+int main() {
+    // TODO: Add your code here
+    
+    return 0;
+}
