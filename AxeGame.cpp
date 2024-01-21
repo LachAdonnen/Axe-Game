a6d2bc301ce180e239c4c5264c06c5517b14ad4a
Alex McClain <alex.mcclain423@gmail.com>
2024-01-21 13:11:45 -0600

    Move circle to the right with D key
    Move the circle left with the A key
    
    Speed up the circle movement and add a speed
    variable.
    
    Keep the circle center within the window bounds

--- AxeGame.cpp
+++ AxeGame.cpp
@@ -18,14 +18,31 @@ int main() {
     int circleX{200};
     int circleY{200};
     int radius{25};
+    int circleMoveSpeed{10};
     
     InitWindow(width, height, "Axe Game");
+    SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
+
+        // Game logic begins
+        
         DrawCircle(circleX, circleY, radius, BLUE);
+        // Respond to user interaction for the circle
+        if (IsKeyDown(KEY_D) && circleX < width)
+        {
+            circleX += circleMoveSpeed;
+        }
+        if (IsKeyDown(KEY_A) && circleX > 0)
+        {
+            circleX -= circleMoveSpeed;
+        }
+
+        // Game logic ends
+
         EndDrawing();
     }
     
